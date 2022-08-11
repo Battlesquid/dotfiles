468a152d810c86c7a1ef4c253b8272dab9c9cd0c
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-08-11 16:03:29 -0700

    fixes for nvim, bspwm, sxhkd

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,29 +3,37 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug ('ryanoasis/vim-devicons')
+Plug ('kyazdani42/nvim-web-devicons')
+
 Plug ('nvim-lua/plenary.nvim')
 Plug ('nvim-telescope/telescope.nvim')
-Plug ('glepnir/dashboard-nvim')
 Plug ('ms-jpq/chadtree', {
 	branch = 'chad', 
 	['do'] = function()
 		vim.cmd('python3 -m chadtree deps')
 	end
 })
-Plug ('glepnir/galaxyline.nvim')
 Plug ('airblade/vim-gitgutter')
-Plug ('kvrohit/substrata.nvim')
-Plug ('karb94/neoscroll.nvim')
-Plug ('Xuyuanp/scrollbar.nvim')
-Plug ('wfxr/minimap.vim')
+Plug ('folke/tokyonight.nvim', { branch = 'main' })
+Plug ('nvim-lualine/lualine.nvim')
+Plug ('romgrk/barbar.nvim')
 
-Plug ('rebelot/kanagawa.nvim')
+Plug ('eraserhd/parinfer-rust', { ['do'] = function()
+		vim.cmd('cargo build --release')
+	end
+})
+Plug ('elkowar/yuck.vim')
+
 vim.call('plug#end')
+
+vim.g.tokyonight_style = "night"
 
 vim.opt.cursorline = true
 vim.cmd [[
-	colorscheme substrata
+	colorscheme tokyonight 
 	set number
+	set mouse+=a
 ]]
 
 require('statusline')
+require('barbar-keybinds')
