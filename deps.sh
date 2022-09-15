4db03b3da11fd772f6bb66a6014eb9538886b0f2
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-09-14 20:55:47 -0700

    add missing scripts/configs

--- deps.sh
+++ deps.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
 paru -S chezmoi xorg bspwm picom-animations-git kitty eww-git rofi dunst redshift pamixer xorg-xinit networkmanager noto-fonts nerd-fonts-noto-sans-mono ttf-font-awesome brightnessctl cava glava neofetch neovim sxhkd betterlockscreen flameshot lxappearance-gtk3 feh playerctl jq recode moreutils jgmenu xcolor
-
-chezmoi init --apply https://github.com/Battlesquid/dotfiles.git
