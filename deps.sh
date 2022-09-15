2136b7f20581ead0e9fb19db3a0317efef8069cb
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-09-15 07:13:46 -0700

    remove lxappearance

--- deps.sh
+++ deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-paru -S chezmoi xorg bspwm picom-animations-git kitty eww-git rofi dunst redshift pamixer xorg-xinit networkmanager noto-fonts nerd-fonts-noto-sans-mono ttf-font-awesome brightnessctl cava glava neofetch neovim sxhkd betterlockscreen flameshot lxappearance-gtk3 feh playerctl jq recode moreutils jgmenu xcolor
+paru -S chezmoi xorg bspwm picom-animations-git kitty eww-git rofi dunst redshift pamixer xorg-xinit networkmanager noto-fonts nerd-fonts-noto-sans-mono ttf-font-awesome brightnessctl cava glava neofetch neovim sxhkd betterlockscreen flameshot feh playerctl jq recode moreutils jgmenu xcolor
